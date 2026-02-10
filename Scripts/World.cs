2626c6a9adf7fa2bcdd89fc262411cd6239fcaf9
slowingwand28 <hum24004@byui.edu>
2026-02-10 15:10:21 -0700

    Figured out how to print to console in godot editor

--- Scripts/World.cs
+++ Scripts/World.cs
@@ -3,16 +3,14 @@ using System;
 
 public partial class World : Node2D
 {
-    [Export] string name;
      // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        Console.WriteLine($"Hello {name}");
+        GD.Print("World Ready");
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
     {
-        Console.WriteLine("Goodbye");
     }
 }
