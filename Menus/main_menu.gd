538ff51f16b2c799d02cc0cc714f8f80a4ecd315
slowingwand28 <hum24004@byui.edu>
2026-03-26 09:36:37 -0600

    Continued work on pause menue

---
+++ Menus/main_menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	get_tree().paused = true
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
